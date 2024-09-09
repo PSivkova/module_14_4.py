59e96f82bff65937a68cf441650f181e0652561e
PSivkova <165670995+PSivkova@users.noreply.github.com>
2024-09-09 23:03:12 +0700

    Update module_14_4.py

--- module_14_4.py
+++ module_14_4.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from crud_functions import *
-api = "7407174524:AAFzd2d9urylslngXjcH7rtXUMOuiNwaQdY"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
